a509eed5ecc25ff07796a0f06acabd5d493949a9
Ivan Delev <2262800d@student.gla.ac.uk>
2019-12-03 20:17:27 +0000

    hiu

--- data/parse_csv.py
+++ data/parse_csv.py
@@ -20,5 +20,25 @@ with open('number-of-deaths-by-risk-factor.csv') as csv_file:
                     mortality[country][year]=dead
 
             line_count += 1
+world_deaths = {}
+with open('deaths _table.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    print(csv_reader)
+    line_count = 0
+    
+    for row in csv_reader:
+        if line_count == 0:
+            #print(f'Column names are {", ".join(row)}')
+            line_count += 1
+            first_row=row
+        else:
+           # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            #print(f'\t{row[0]} in {row[2]} year from smoking died {row[23]}.')
+            country = row[0]
+            deaths={}
+            for i in range(1,len(row)):
+                deaths.update({first_row[i]:row[i]})
+            world_deaths.update({country:deaths})
+            line_count += 1
     print(f'Processed {line_count} lines.')
     print(mortality)
--- python/worlddeaths.py
+++ python/worlddeaths.py
@@ -1,19 +1,22 @@
 import csv
 world_deaths = {}
-with open('deaths_table.csv') as csv_file:
+with open('deaths _table.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
+    print(csv_reader)
     line_count = 0
+    
     for row in csv_reader:
         if line_count == 0:
             #print(f'Column names are {", ".join(row)}')
             line_count += 1
+            first_row=row
         else:
            # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
             #print(f'\t{row[0]} in {row[2]} year from smoking died {row[23]}.')
             country = row[0]
             deaths={}
             for i in range(1,len(row)):
-                deaths.update({csv_reader[0][i]:float(row[i])})
+                deaths.update({first_row[i]:row[i]})
             world_deaths.update({country:deaths})
             line_count += 1
 print(world_deaths)
