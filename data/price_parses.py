69c1440150de2b7cf88dd13a3158dbe59f11b752
vikinan97 <vikinankova97@gmail.com>
2019-12-03 17:53:40 +0000

    price data

--- data/price_parses.py
+++ data/price_parses.py
@@ -1,17 +1,17 @@
 import json
 with open("tobacco_prices.json", 'r', encoding="utf-8") as f:
-        datastore = json.load(f)
-        dic={}
-        for el in datastore["fact"]:
+         datastore = json.load(f)
+         dic={}
+         for el in datastore["fact"]:
             if el["dim"]["GHO"]=="Most sold brand of cigarettes - price in US$ at official exchange rates":
-                country=el["dim"]["COUNTRY"]
-                year=el["dim"]["YEAR"]
-                val=float(el["Value"])
-                if country not in dic:
-                   dic.update({country:{year:val}})
-                else:
-                   dic[country][year]=val
-						
-                
-        #print(dic)
+               country=el["dim"]["COUNTRY"]
+               year=el["dim"]["YEAR"]
+               try:
+                  val=float(el["Value"])
+               except:
+                  val=""
+               if country not in dic:
+                  dic.update({country:{year:val}})
+               else:
+                  dic[country][year]=val
 print(json.dumps(dic))
